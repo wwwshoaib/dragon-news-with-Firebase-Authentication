8d783d69abdccd728ce040d713c9eaff571245c9
Shoaib <shoaibchemistrysust@gmail.com>
2025-03-06 10:12:33 +0600

    Categories loaded and displayed on UI

--- src/components/layout-components/LeftNavbar.jsx
+++ src/components/layout-components/LeftNavbar.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 
+/**
+ * 
+ *   {
+        "category_id": "01",
+        "category_name": "Breaking News"
+      },
+ */
 
 
 const LeftNavbar = () => {
+    //load categories
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch("https://openapi.programming-hero.com/api/news/categories")
+            .then(res => res.json())
+            .then(data => setCategories(data.data.news_category
+            ))
+    }, [])
     return (
         <div>
-            <h2>Left Navbar</h2>
-            
+            <h2 className="font-semibold text-[#444444 text-center">All Categories ({categories.length})</h2>
+            <div className="flex flex-col gap-2 mt-3">
+                {
+                    categories.map(category => <button
+                        className="btn bg-base-200"
+                        key={category.category_id}>{category.category_name}
+                    </button>)
+                }
+            </div>
+
+
         </div>
     );
 };
